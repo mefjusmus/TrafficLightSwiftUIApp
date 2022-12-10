fc0b9060911caae96ae49bac53a173759d44ca58
Vladislav <vsuslov7@gmail.com>
2022-12-10 14:33:04 +0300

    Initial Commit

---
+++ TrafficLightSwiftUIApp/TrafficLightSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIAppApp.swift
+//  TrafficLightSwiftUIApp
+//
+//  Created by Vladislav Suslov on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
